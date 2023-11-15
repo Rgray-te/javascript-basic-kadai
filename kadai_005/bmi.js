4cc864dbb4f1a2a6cd6958428da83b52a5c0870a
Rgray-te <148609108+Rgray-te@users.noreply.github.com>
2023-11-15 22:48:34 +0900

    kadai_005を追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重の変数を宣言・値の代入
+let bodyWeight = 68;
+
+// 身長の変数を宣言・値の代入
+let height = 1.7;
+
+// BMIの変数を宣言・値の代入
+let BMI = bodyWeight / height * height;
+
+// BMIを出力するプログラムを記述
+console.log(BMI);
